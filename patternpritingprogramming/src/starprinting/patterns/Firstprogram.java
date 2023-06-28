85b984ac7195722ba8f826560c92472d873247a9
vishal <vishal.thorat8860@gmail.com>
2023-06-28 22:53:31 +0530

    logic and pattern printing

---
+++ patternpritingprogramming/src/starprinting/patterns/Firstprogram.java
@@ -0,0 +1,14 @@
+package starprinting.patterns;
+
+public class Firstprogram {
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		int c=a+b;
+		System.out.println(c);
+		
+	}
+
+}
